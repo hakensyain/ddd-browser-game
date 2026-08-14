1b90df75ad646146851a471586f55af5b458f5ed
hakensyain <270924870+hakensyain@users.noreply.github.com>
2026-08-14 22:28:59 +0900

    Update browser trial build

--- assets/propagandaScenarios-BBkgHmzp.js
+++
@@ -1,1 +0,0 @@
-import{a as e,i as t}from"./index-TsvOC-x7.js";export{t as getPropagandaScenario,e as getPropagandaScenarioCount};
---
+++ assets/propagandaScenarios-DNolZU20.js
@@ -0,0 +1,1 @@
+import{a as e,i as t}from"./index-BnNUa50x.js";export{t as getPropagandaScenario,e as getPropagandaScenarioCount};
